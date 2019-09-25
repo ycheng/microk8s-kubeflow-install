7d1f68e5f1d1c10c6c45de6fca644125a8e83d2e
Yuan-Chen Cheng <ycheng.tw@gmail.com>
2019-09-25 12:14:34 +0800

    fix microk8s-install.sh, it should works now.

--- microk8s-install.bash
+++ microk8s-install.bash
@@ -4,11 +4,16 @@ set -x
 set -e
 
 sudo snap install microk8s --classic
+
 sudo microk8s.status --wait-ready
+sudo microk8s.kubectl config view --raw | sudo tee -a $HOME/.kube/config
+sudo -E chown -R `id -nu`.`id -ng` $HOME/.kube
+
 sudo microk8s.enable dns storage dashboard
 sudo microk8s.status --wait-ready
 sudo snap alias microk8s.kubectl kubectl
 
-microk8s.kubectl config view --raw > $HOME/.kube/config
+sudo usermod -a -G microk8s `id -nu`
 
-sudo usermod -a -G microk8s $USER
+echo You are added to group microk8s, so please logout and login so it works.
+echo Done
